33f8c35ce58c7b7e862ea2732dc0d44e9bc21ba0
jagadeesh-nj <jagadeesh88.n@gmail.com>
2020-03-15 20:25:05 +0530

    changeesmaven

---
+++ src/main/java/qatesting/Testing.java
@@ -0,0 +1,14 @@
+package qatesting;
+
+public class Testing {
+
+	public void car(){
+		
+	}
+	/*
+	public static void main(String[] args) {
+		System.out.println("hello maaven");
+
+	}
+*/
+}
---
+++ src/test/java/qatesting/Testcheck.java
@@ -0,0 +1,15 @@
+package qatesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class Testcheck {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
