861374d373e88d5998be7a6f028996c120764790
jagadeesh-nj <jagadeesh88.n@gmail.com>
2020-04-18 18:44:51 +0530

    adding the projecct

--- src/main/java/qatesting/Testing.java
+++ src/main/java/qatesting/Testing.java
@@ -1,14 +1,40 @@
 package qatesting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Testing {
 
-	public void car(){
+public static void main(String[] args) {
 		
-	}
-	/*
-	public static void main(String[] args) {
-		System.out.println("hello maaven");
+		ArrayList<Integer> ar = new ArrayList<Integer>();
+		ArrayList<String> as = new ArrayList<String>(
+				Arrays.asList("jagadeesh ", "satish " , "ravi"));
+		ar.add(1);
+		ar.add(567);
+		ar.add(567);
+		ar.add(2 ,345);
+		as.add(0 , "Steve");
+		System.out.println(as);
+		System.out.println(ar);
+		System.out.println(ar.get(1));
+		
+		
+		System.out.println(as);
+		
+		/*for(String s : as){
+			System.out.println(s);
+		}
+		
+		Iterator it = as.iterator();
+		it.hasNext();
+		System.out.println(it.next());*/
+		
+		Collections.sort(as);
+		for(String s : as){
+			System.out.println(s);
+		}
+}
 
-	}
-*/
 }
